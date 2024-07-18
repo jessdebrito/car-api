a07f86beb9f0a10c7f9fd4ca182e52cb13d505ae
jessdebrito <debrito.herself@gmail.com>
2024-07-17 22:42:02 -0300

    car_and_account_table

---
+++ prisma/migrations/20240718014129_car_and_account_table/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Car" (
+    "id" SERIAL NOT NULL,
+    "car_name" VARCHAR(100) NOT NULL,
+    "description" TEXT,
+    "brand" VARCHAR(100) NOT NULL,
+    "year" INTEGER NOT NULL,
+    "km" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "full_name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
