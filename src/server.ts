a88dd8f9a5fe13ee46b5ab3c11ec5bdd35c39726
jessdebrito <debrito.herself@gmail.com>
2024-07-17 23:13:41 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { app } from "./app";
 
-const PORT = process.env.API_PORT || 3000;
+const PORT = process.env.API_PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
