60dcf4c63e8bafeea08b674aabd57d9eedbe2b2f
jessdebrito <debrito.herself@gmail.com>
2024-07-18 02:37:59 -0300

    .

--- src/api/account/schemas.ts
+++ src/api/account/schemas.ts
@@ -2,12 +2,13 @@ import { z } from "zod";
 
 export const accountSchema = z.object({
     id: z.number().positive().int(),
-    name: z.string().max(100),
+    fullName: z.string().max(100),
     email: z.string().email().max(100),
     password: z.string().max(255),
     createdAt: z.date(),
     updatedAt: z.date(),
 });
+
 export const accountCreateSchema = accountSchema.omit({
     id: true,
     createdAt: true,
--- src/api/session/service.ts
+++ src/api/session/service.ts
@@ -5,20 +5,23 @@ import * as bcrypt from "bcryptjs";
 
 export class SessionService {
     public login = async (payload: SessionLogin) => {
-        const account = await prisma.account.findUnique({ 
+        const account = await prisma.account.findUnique({
             where: { email: payload.email },
-    });
+        });
 
-    if(!account) {
-        throw new ApiError("E-mail and password doesn't match", 401)
-    }
+        if (!account) {
+            throw new ApiError("Invalid credentials", 401)
+        };
 
-    const passwordMatch = await bcrypt.compare(
-        payload.password,
-         account.password
+        const passwordMatch = await bcrypt.compare(
+            payload.password,
+            account.password
         );
 
+        if (!passwordMatch) {
+            throw new ApiError("Invalid credentials", 401)
+        };
 
-        return ({ message: "POST /login"});
+        return ({ message: "POST /login" });
     };
 }
